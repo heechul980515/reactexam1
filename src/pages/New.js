18e07d9566d9707b587b84fc7b09a0fd00bf77d2
heechul980515 <121490819+heechul980515@users.noreply.github.com>
2023-03-20 02:52:09 +0900

    20230320

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,10 +1,11 @@
+import DiaryEditor from "../components/DiaryEditor";
+
 const New = () =>{
     return (
         <div>
-            <h1>New</h1>
-            <p>이곳은 일기 추가 페이지 입니다.</p>
+            <DiaryEditor/>
         </div>
-    );
+    )
 };
 
 export default New;
